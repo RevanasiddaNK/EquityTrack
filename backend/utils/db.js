23b9dd7c8294bc34b2193b9a1e33ca9e95883a94
RevanasiddaNK <nmrevanasiddarhbk@gmail.com>
2025-01-14 00:53:42 +0530

    Deployment configuration added

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 const connectDB = async ()=> {
     try{
-        console.log(process.env.MONGO_URI)
         await mongoose.connect(process.env.MONGO_URI);
         console.log("mongoDB connected successfully");
     }catch(error){
