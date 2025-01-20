8d1bd2413e9966bd9a230622aef4c94360a14490
RevanasiddaNK <nmrevanasiddarhbk@gmail.com>
2025-01-20 10:14:26 +0530

    update fetchStockData

--- backend/utils/data.js
+++ backend/utils/data.js
@@ -1,128 +1,9 @@
 import {DailyStock} from '../models/dailyStock.model.js';
+import { Stock } from '../models/stock.model.js';
 import connectDB from './db.js';
 
-const stockDetails = [
-  {
-      "ticker": "RELIANCE.BSE",
-      "name": "Reliance Industries Limited",
-      "date": "2025-01-14",
-      "open": "2340.7500",
-      "high": "2360.1200",
-      "low": "2320.4720",
-      "close": "2330.2800",
-      "volume": "39435294"
-  },
-  {
-      "ticker": "TCS.BSE",
-      "name": "Tata Consultancy Services Limited",
-      "date": "2025-01-14",
-      "open": "4170.8100",
-      "high": "4190.7400",
-      "low": "4100.7200",
-      "close": "4150.6700",
-      "volume": "16935856"
-  },
-  {
-      "ticker": "INFY.BSE",
-      "name": "Infosys Limited",
-      "date": "2025-01-14",
-      "open": "1910.2400",
-      "high": "1919.9800",
-      "low": "1888.3082",
-      "close": "1899.6600",
-      "volume": "17174854"
-  },
-  {
-      "ticker": "HDFC.BSE",
-      "name": "Housing Development Finance Corporation",
-      "date": "2025-01-14",
-      "open": "2200.4400",
-      "high": "2210.8200",
-      "low": "2160.2000",
-      "close": "2170.7600",
-      "volume": "24711650"
-  },
-  {
-      "ticker": "ICICIBANK.BSE",
-      "name": "ICICI Bank Limited",
-      "date": "2025-01-14",
-      "open": "4140.3400",
-      "high": "4220.6400",
-      "low": "3940.5400",
-      "close": "3960.3600",
-      "volume": "83216061"
-  },
-  {
-      "ticker": "SBIN.BSE",
-      "name": "State Bank of India",
-      "date": "2025-01-14",
-      "open": "8430.2000",
-      "high": "8440.8925",
-      "low": "8230.5190",
-      "close": "8280.4000",
-      "volume": "3037650"
-  },
-  {
-      "ticker": "HINDUNILVR.BSE",
-      "name": "Hindustan Unilever Limited",
-      "date": "2025-01-14",
-      "open": "6050.0650",
-      "high": "6050.4900",
-      "low": "5880.5500",
-      "close": "5940.2500",
-      "volume": "13597992"
-  },
-  {
-      "ticker": "ADANIENT.BSE",
-      "name": "Adani Enterprises Limited",
-      "date": "2025-01-14",
-      "open": "1360.0500",
-      "high": "1363.3800",
-      "low": "1300.0500",
-      "close": "1317.7600",
-      "volume": "195590485"
-  },
-  {
-      "ticker": "ITC.BSE",
-      "name": "ITC Limited",
-      "date": "2025-01-14",
-      "open": "4450.5000",
-      "high": "4503.0000",
-      "low": "4433.9200",
-      "close": "4500.3000",
-      "volume": "4055602"
-  },
-  {
-      "ticker": "LT.BSE",
-      "name": "Larsen & Toubro Limited",
-      "date": "2025-01-14",
-      "open": "2420.6600",
-      "high": "2477.2000",
-      "low": "2421.9000",
-      "close": "2474.7000",
-      "volume": "12407386"
-  },
-  {
-      "ticker": "BAJFINANCE.BSE",
-      "name": "Bajaj Finance Limited",
-      "date": "2025-01-14",
-      "open": "3087.5000",
-      "high": "3101.6000",
-      "low": "3075.2000",
-      "close": "3090.9000",
-      "volume": "5107901"
-  },
-  {
-      "ticker": "KOTAKBANK.BSE",
-      "name": "Kotak Mahindra Bank Limited",
-      "date": "2025-01-14",
-      "open": "1086.8000",
-      "high": "1089.6000",
-      "low": "1076.1000",
-      "close": "1081.2000",
-      "volume": "5501600"
-  }
-];
+
+
 /*
 connectDB();
 await DailyStock.deleteMany({})
@@ -132,7 +13,9 @@ console.log(data);
 
 
 export const fetchStockData = async () => {
+  
   try {
+  const stockDetails = await Stock.find({});
     await Promise.all(
       stockDetails.map(async (stock) => {
         try {
