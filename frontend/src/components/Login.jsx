bf2a2fce79d1e5d4b29d62d680045270a7ba1abe
RevanasiddaNK <nmrevanasiddarhbk@gmail.com>
2025-01-23 11:40:55 +0530

    upadate loginPage

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -77,7 +77,7 @@ const Login = () => {
                 name="email"
                 onChange={changeEventHandler}
                 className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200"
-                placeholder="Enter your email"
+                placeholder="user@gmail.com"
                 required
             />
             </div>
@@ -94,7 +94,7 @@ const Login = () => {
                 name="password"
                 onChange={changeEventHandler}
                 className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200"
-                placeholder="Enter your password"
+                placeholder="user@123"
                 required
             />
             </div>
